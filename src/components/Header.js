5aaf3d58abd3fdc769b5cfb309e1f1b06bfade5a
собака <erazmozen@protonmail.com>
2022-06-23 17:32:37 +0200

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import trollFace from "../images/troll-face.png";
+
+export default function Header() {
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <img src={trollFace} className="header--image" />
+        <h2 className="header--title">Meme Generator</h2>
+      </header>
+    </div>
+  );
+}
